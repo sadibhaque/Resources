464c0b4ceba891211905503160a7b4e9d2865753
Sadiqul Haque Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-09-03 06:59:59 -0700

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,1 +1,11 @@
-//
+cat << "EOF"
+
+Setting Up Dotfiles :                                     
+                                        
+EOF
+
+gti clone https://github.com/snadipsky/dotfiles
+cd dotfiles/hyprland/config/
+
+cp -r hypr rofi waybar /home/mendax/.config/
+cd /home/mendax/.config/
