8b54c8b87456bb9733e357ebef224c370d7dcb09
Sadiqul Haque Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-09-03 06:19:25 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,46 @@
-sudo pacman -S git hyprland waybar dunst hyprpaper hypridle xdg-desktop-portal-hyprland xdg-user-dirs-gtk rofi-wayland noto-fonts ttf-fira-sans ttf-font-awesome lxappearance libadwaita brightnessctl kitty alacritty sddm hyprlock firefox vlc mousepad thunar file-roller thunar-archive-plugin tumbler ffmpegthumbnailer gvfs-mtp galculator ristretto
+cat << "EOF"
+
+  __  __                _            
+ |  \/  |              | |           
+ | \  / | ___ _ __   __| | __ ___  __
+ | |\/| |/ _ \ '_ \ / _` |/ _` \ \/ /
+ | |  | |  __/ | | | (_| | (_| |>  < 
+ |_|  |_|\___|_| |_|\__,_|\__,_/_/\_\
+                                     
+                                
+EOF
+
+cat << "EOF"
+
+Installing Necessary Software :                                     
+                                        
+EOF
+
+sudo pacman -S hyprland waybar dunst hyprpaper hypridle xdg-desktop-portal-hyprland xdg-user-dirs-gtk rofi-wayland noto-fonts ttf-fira-sans ttf-font-awesome lxappearance libadwaita brightnessctl kitty alacritty sddm hyprlock firefox vlc mousepad thunar file-roller thunar-archive-plugin tumbler ffmpegthumbnailer gvfs-mtp galculator ristretto
+
+cat << "EOF"
+
+Setting Up Dotfiles :                                     
+                                        
+EOF
+
+gti clone https://github.com/snadipsky/dotfiles
+cd dotfiles/hyprland/config/
+
+cp -r hypr rofi waybar /home/mendax/.config/
+
+cat << "EOF"
+
+Installing YAY repo :                                     
+                                        
+EOF
+
+gti clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -sri
+
+cat << "EOF"
+
+Initial Setup Done !                                     
+                                        
+EOF
