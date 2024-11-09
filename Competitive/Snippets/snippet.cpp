5bac6ee23d0f259a3f3632288778e4063c4593c1
Sadiqul Haque Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-11-09 13:45:03 +0600

    Create snippet.cpp

---
+++ Competitive/Snippets/snippet.cpp
@@ -0,0 +1,402 @@
+{
+    "C++ Snippet": {
+        "prefix": "q",
+        "body": [
+            "#include \"bits/stdc++.h\"",
+            "#include \"ext/pb_ds/assoc_container.hpp\"",
+            "#include \"ext/pb_ds/tree_policy.hpp\"",
+            "",
+            "using namespace std;",
+            "using namespace __gnu_pbds;",
+            "",
+            "#ifdef MENDAX",
+            "#include \"DEBUG_TEMPLATE.h\"",
+            "#else",
+            "#define HERE",
+            "#define debug(args...)",
+            "#endif",
+            "",
+            "#define int long long",
+            "#define lcm(a, b) ((a * b) / __gcd(a, b));",
+            "",
+            "template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;",
+            "",
+            "",
+            "void S_1984() {",
+            "\t$0",
+            "}",
+            "",
+            "signed main() {",
+            "#ifndef MENDAX",
+            "\tios_base::sync_with_stdio(0); cin.tie(0);",
+            "#endif",
+            "#ifndef ONLINE_JUDGE",
+            "\tfreopen(\"input.txt\", \"r\", stdin);",
+            "#endif",
+            "\tint t = 1; cin >> t;",
+            "\twhile (t--) S_1984();",
+            "#ifndef ONLINE_JUDGE",
+            "\tprintf(\"Time: %lld ms\\n\", (int)(clock() * 1000. / CLOCKS_PER_SEC));",
+            "#endif",
+            "\treturn 0;",
+            "}"
+        ],
+    },
+    "C++ Snippetq": {
+        "prefix": "qq",
+        "body": [
+            "#include \"bits/stdc++.h\"",
+            "#include \"ext/pb_ds/assoc_container.hpp\"",
+            "#include \"ext/pb_ds/tree_policy.hpp\"",
+            "",
+            "using namespace std;",
+            "using namespace __gnu_pbds;",
+            "",
+            "#ifdef MENDAX",
+            "#include \"DEBUG_TEMPLATE.h\"",
+            "#else",
+            "#define HERE",
+            "#define debug(args...)",
+            "#endif",
+            "",
+            "#define int long long",
+            "#define lcm(a, b) ((a * b) / __gcd(a, b));",
+            "",
+            "template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;",
+            "",
+            "",
+            "void S_1984() {",
+            "\t$0",
+            "}",
+            "",
+            "signed main() {",
+            "#ifndef MENDAX",
+            "\tios_base::sync_with_stdio(0); cin.tie(0);",
+            "#endif",
+            "#ifndef ONLINE_JUDGE",
+            "\tfreopen(\"input.txt\", \"r\", stdin);",
+            "#endif",
+            "\tS_1984();",
+            "#ifndef ONLINE_JUDGE",
+            "\tprintf(\"Time: %lld ms\\n\", (int)(clock() * 1000. / CLOCKS_PER_SEC));",
+            "#endif",
+            "\treturn 0;",
+            "}"
+        ],
+    },
+    "Fori Snippet": {
+        "prefix": "fori",
+        "body": [
+            "for (int i = 0; i < $1; i++) $0"
+        ],
+    },
+    "Forij Snippet": {
+        "prefix": "forij",
+        "body": [
+            "for (int i = 0; i < $1; i++) {",
+            "\tfor (int j = $2; j < $3; j++) {",
+            "\t\t$4",
+            "\t}",
+            "}",
+        ],
+    },
+    "Fori- Snippet": {
+        "prefix": "fori-",
+        "body": [
+            "for (int i = $1; i >= 0; i--) $0"
+        ],
+    },
+    "Forj- Snippet": {
+        "prefix": "forj-",
+        "body": [
+            "for (int j = $1; j >= 0; j--) $0"
+        ],
+    },
+    "Forj Snippet": {
+        "prefix": "forj",
+        "body": [
+            "for (int j = 0; j < $1; j++) $0"
+        ],
+    },
+    "Fork Snippet": {
+        "prefix": "fork",
+        "body": [
+            "for (int k = 0; k < $1; k++) $0"
+        ],
+    },
+    "arr print Snippet": {
+        "prefix": "parr",
+        "body": [
+            "for (int i = 0; i < $1; i++) {",
+            "\tcout << arr[i] << \" \";",
+            "}",
+        ],
+    },
+    "For it Snippet": {
+        "prefix": "fora",
+        "body": [
+            "for (auto it : $1) $0"
+        ],
+    },
+    "For ch Snippet": {
+        "prefix": "forch",
+        "body": [
+            "for (auto ch : $1) $0"
+        ],
+    },
+    "For xy Snippet": {
+        "prefix": "forxy",
+        "body": [
+            "for (auto [x,y] : $1) $0"
+        ],
+    },
+    "vectori": {
+        "prefix": "vi",
+        "body": [
+            "vector <int> "
+        ],
+    },
+    "vectorii": {
+        "prefix": "vvi",
+        "body": [
+            "vector <vector<int>> "
+        ],
+    },
+    "vectorl": {
+        "prefix": "vl",
+        "body": [
+            "vector <long long> "
+        ],
+    },
+    "mapi": {
+        "prefix": "mpi",
+        "body": [
+            "map <int, int> "
+        ],
+    },
+    "mapc": {
+        "prefix": "mpc",
+        "body": [
+            "map <char, int> "
+        ],
+    },
+    "long": {
+        "prefix": "ll",
+        "body": [
+            "long long "
+        ],
+    },
+    "umapi": {
+        "prefix": "umpi",
+        "body": [
+            "unordered_map <int, int> "
+        ],
+    },
+    "umapc": {
+        "prefix": "umpc",
+        "body": [
+            "unordered_map <char, int> "
+        ],
+    },
+    "pb": {
+        "prefix": "pbk",
+        "body": [
+            "push_back($0)"
+        ],
+    },
+    "pairi": {
+        "prefix": "pii",
+        "body": [
+            "pair <int, int> "
+        ],
+    },
+    "pairl": {
+        "prefix": "pil",
+        "body": [
+            "pair <long long, long long> "
+        ],
+    },
+    "first": {
+        "prefix": "f",
+        "body": [
+            "first"
+        ],
+    },
+    "second": {
+        "prefix": "s",
+        "body": [
+            "second"
+        ],
+    },
+    "ans": {
+        "prefix": "pans",
+        "body": [
+            "cout << ans << '\\n';"
+        ],
+    },
+    "sum": {
+        "prefix": "psum",
+        "body": [
+            "cout << sum << '\\n';"
+        ],
+    },
+    "ctr": {
+        "prefix": "pctr",
+        "body": [
+            "cout << ctr << '\\n';"
+        ],
+    },
+    "end": {
+        "prefix": "pend",
+        "body": [
+            "cout <<'\\n';"
+        ],
+    },
+    "it": {
+        "prefix": "pit",
+        "body": [
+            "cout << it << ' ';"
+        ],
+    },
+    "yes": {
+        "prefix": "py",
+        "body": [
+            "cout << \"YES\\n\";"
+        ],
+        "description": "Outputs Yes followed by a newline"
+    },
+    "no": {
+        "prefix": "pn",
+        "body": [
+            "cout << \"NO\\n\";"
+        ],
+        "description": "Outputs Yes followed by a newline"
+    },
+    "cout1": {
+        "prefix": "c",
+        "body": [
+            "cout << $0 << '\\n';"
+        ],
+        "description": "Outputs Yes followed by a newline"
+    },
+    "cout2": {
+        "prefix": "cc",
+        "body": [
+            "cout << $0 << ' ';"
+        ],
+        "description": "Outputs Yes followed by a newline"
+    },
+    "vpl": {
+        "prefix": "vpl  ",
+        "body": [
+            "vector <pair<long long,long long>> $0"
+        ],
+    },
+    "arc int": {
+        "prefix": "arc",
+        "body": [
+            "int n; ",
+            "cin >> n;",
+            "",
+            "vector <int> arr(n);",
+            "for (int i = 0; i < n; i++) {",
+            "\tcin >> arr[i];",
+            "}",
+            "",
+            "$0",
+        ],
+    },
+    "src": {
+        "prefix": "src",
+        "body": [
+            "string str;",
+            "cin >> str;",
+            "",
+            "$0",
+        ],
+    },
+    "srcn": {
+        "prefix": "srcn",
+        "body": [
+            "int n; ",
+            "cin >> n;",
+            "string str;",
+            "cin >> str;",
+            "",
+            "$0",
+        ],
+    },
+    "arc ll": {
+        "prefix": "arcl",
+        "body": [
+            "long long n; ",
+            "cin >> n;",
+            "",
+            "vector <long long> arr(n);",
+            "for (long long i = 0; i < n; i++) {",
+            "\tcin >> arr[i];",
+            "}",
+            "",
+            "$0",
+        ],
+    },
+    "arc k": {
+        "prefix": "arck",
+        "body": [
+            "int n, k; ",
+            "cin >> n >> k;",
+            "",
+            "vector <int> arr(n);",
+            "for (int i = 0; i < n; i++) {",
+            "\tcin >> arr[i];",
+            "}",
+            "",
+            "$0",
+        ],
+    },
+    "arc kl": {
+        "prefix": "arckl",
+        "body": [
+            "long long n, k; ",
+            "cin >> n >> k;",
+            "",
+            "vector <long long> arr(n);",
+            "for (long long i = 0; i < n; i++) {",
+            "\tcin >> arr[i];",
+            "}",
+            "",
+            "$0",
+        ],
+    },
+    "arc nm": {
+        "prefix": "arcnm",
+        "body": [
+            "int n, m; ",
+            "cin >> n >> m;",
+            "",
+            "vector <int> arr(n), brr(m);",
+            "for (int i = 0; i < n; i++) cin >> arr[i];",
+            "for (int i = 0; i < m; i++) cin >> brr[i];",
+            "",
+            "$0",
+        ],
+    },
+    "arr sort": {
+        "prefix": "srt",
+        "body": [
+            "sort($0.begin(), $0.end());",
+        ],
+    },
+    "arr rev": {
+        "prefix": "rev",
+        "body": [
+            "reverse($0.begin(), $0.end());",
+        ],
+    },
+    "freopen": {
+        "prefix": "fpn",
+        "body": [
+            "freopen(\"input.txt\", \"r\", stdin);",
+        ],
+    },
+}
